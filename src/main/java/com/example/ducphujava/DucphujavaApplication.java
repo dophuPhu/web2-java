cf2bb04d78cbf57502b31f6794ae854c45042978
dophuPhu <dophu0304@gmail.com>
2025-02-24 10:34:58 +0700

    first commit

---
+++ src/main/java/com/example/ducphujava/DucphujavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ducphujava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucphujavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DucphujavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ducphujava/DucphujavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ducphujava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucphujavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
