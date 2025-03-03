7294deffae6428666fc2c75a2cc7d601a177f80a
dophuPhu <dophu0304@gmail.com>
2025-03-03 11:02:40 +0700

    Cập nhật HelloController - thêm API DELETE

--- src/main/java/com/example/ducphujava/DucphujavaApplication.java
+++ src/main/java/com/example/ducphujava/DucphujavaApplication.java
@@ -8,6 +8,7 @@ public class DucphujavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DucphujavaApplication.class, args);
+		System.out.println("Hello World"); 
 	}
 
 }
