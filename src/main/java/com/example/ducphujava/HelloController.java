2ab6541d4646b2c27e2f2f845f57cf7de5abebaa
dophuPhu <dophu0304@gmail.com>
2025-03-10 10:52:53 +0700

    Mô tả ngắn gọn về thay đổi

--- src/main/java/com/example/ducphujava/HelloController.java
+++ src/main/java/com/example/ducphujava/HelloController.java
@@ -1,40 +1,40 @@
-package com.example.ducphujava;
+    package com.example.ducphujava;
 
-import org.springframework.web.bind.annotation.*;
+    import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api")
-public class HelloController {
+    @RestController
+    @RequestMapping("/api")
+    public class HelloController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World";
+        @GetMapping("/hello")
+        public String sayHello() {
+            return "Hello World";
+        }
+
+        @PostMapping("/sum")
+        public int sumNumbers(@RequestBody SumRequest request) {
+            return request.getNumber1() + request.getNumber2();
+        }
     }
 
-    @PostMapping("/sum")
-    public int sumNumbers(@RequestBody SumRequest request) {
-        return request.getNumber1() + request.getNumber2();
-    }
-}
+    // DTO để nhận dữ liệu từ request body
+    class SumRequest {
+        private int number1;
+        private int number2;
 
-// DTO để nhận dữ liệu từ request body
-class SumRequest {
-    private int number1;
-    private int number2;
+        public int getNumber1() {
+            return number1;
+        }
 
-    public int getNumber1() {
-        return number1;
-    }
+        public void setNumber1(int number1) {
+            this.number1 = number1;
+        }
 
-    public void setNumber1(int number1) {
-        this.number1 = number1;
-    }
+        public int getNumber2() {
+            return number2;
+        }
 
-    public int getNumber2() {
-        return number2;
+        public void setNumber2(int number2) {
+            this.number2 = number2;
+        }
     }
-
-    public void setNumber2(int number2) {
-        this.number2 = number2;
-    }
-}
